81b5c0bf840c4e2c1d70a30a047e177a4940f8eb
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-13 09:05:00 +0530

    add utitlity classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+	const [isOpen, setIsOpen] = useState(false);
+
 	return (
 		<div>
 			<nav>
 				<h1>Logo</h1>
-				<ul>
+				<ul className={isOpen ? "menu-open" : ""}>
 					<li>Home</li>
 					<li>About</li>
 					<li>Contact</li>
 					<li>Help</li>
 					<li>Sign-In</li>
 				</ul>
-				<button className="menu-icon">☰</button>
+				<button className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+					☰
+				</button>
 			</nav>
 		</div>
 	);
